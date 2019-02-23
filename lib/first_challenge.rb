e044d929cddff32276ca9a373b353a8bd07da8a7
John Pena <jpena91@gmail.com>
2019-02-23 18:04:28 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,16 +1,5 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {
-       :patriarch => {},
-       :matriarch => {},
-       :hero => {},
-       :hero_friends => [ ]
-     },
-     :capulate => {
-       :patriarch => {},
-       :matriarch => {},
-       :heroine => {},
-       :heroine_friends = [ ]
-     }
-   }
+     :montague => {},
+     :capulate => {}
 end
